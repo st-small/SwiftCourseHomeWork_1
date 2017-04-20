4ca034f1e02893ff05f0dda0a319213e986ab0d6
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-04-20 11:28:33 +0300

    Сделал задание 2

--- SwiftCourseHomeWork_1.playground/Contents.swift
+++ SwiftCourseHomeWork_1.playground/Contents.swift
@@ -5,31 +5,49 @@ import UIKit
 
 func integerValues(A: Int, B: Int, N: Int) {
     var array = [Int]()
-    if A > 0, B > 0, N > 0 {
-        for integer in A...B {
-            var count: Int = 0
-            var num: Int = integer
-            while num > 0 {
-                let digit = num % 10
-                if digit > 0 && digit == 7 {
-                    count += 1
-                }
-                num = num / 10
+    guard A > 0, B > 0, N > 0 else {
+        return
+    }
+    for integer in A...B {
+        var count: Int = 0
+        var num: Int = integer
+        while num > 0 {
+            if num % 10 == 7 {
+                count += 1
             }
-            if count == N {
-                array.append(integer)
-                print(integer)
-            }
+            num = num / 10
         }
-        if array.count == 0 {
-            print("Требуемых значений не найдено")
+        
+        if count == N {
+            array.append(integer)
+            print(integer)
         }
+    }
+    if array.count == 0 {
+        print("Требуемых значений не найдено")
     }
 }
 
 // Проверка работы Задания 1
-integerValues(A: 1, B: 5555, N: 3)
+integerValues(A: 1, B: 55, N: 1)
 
 // Задание 2
+func reverese(A: Int) {
+    guard A >= 100, A <= 999  else {
+        print("Число не трехзначное")
+        return
+    }
+    var old: Int = A
+    var new: Int = 0
+    for index in 1...3 {
+        let powResult = NSDecimalNumber(decimal: pow(10, 3-index))
+        new = new + (old%10 * Int(powResult))
+        old = old/10
+    }
+    
+    print(new)
+}
 
+// Проверка работы Задания 2
+reverese(A: 123)
 
