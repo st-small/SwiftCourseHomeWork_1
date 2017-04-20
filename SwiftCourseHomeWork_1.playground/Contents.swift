f294db10767bcc560aee508bacf5bdc09148cde1
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-04-20 20:44:45 +0300

    Закончил задание 3

--- SwiftCourseHomeWork_1.playground/Contents.swift
+++ SwiftCourseHomeWork_1.playground/Contents.swift
@@ -51,3 +51,20 @@ func reverese(A: Int) {
 // Проверка работы Задания 2
 reverese(A: 123)
 
+// Задание 3
+func luckyNumber(A: Double) {
+    let check: Int = Int(Double(A) * 100)
+    guard check >= 1001, check <= 9999 else {
+        print("Число не подходит под формат ХХ,ХХ")
+        return
+    }
+    var first: Int = 0
+    var second: Int = 0
+    first = check / 1000 + (check / 100 - (check / 1000) * 10)
+    second = (check % 100) / 10 + check % 10
+    first == second ? print("Поздравляем со счастливым билетом!!!") : print("Попробуйте еще")
+}
+
+// Проверка работы задания 3
+luckyNumber(A: 22.4)
+
